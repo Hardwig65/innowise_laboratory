f5bcece8b911a6dc3989ce61f8c020904a11d651
Egor <Hardwig65@gmail.com>
2025-11-20 18:01:19 +0100

    add user input and hobbies loop

--- lecture_2/main.py
+++ lecture_2/main.py
@@ -8,3 +8,23 @@ def generate_profile(age: int) -> str:
         return "Adult"
     else:
         return "Invalid age"
+
+
+# Get user input
+user_name = input("Enter your full name: ")
+birth_year_str = input("Enter your birth year: ")
+birth_year = int(birth_year_str)
+
+# Calculate current age
+from datetime import datetime
+
+current_year = datetime.now().year
+current_age = current_year - birth_year
+
+# Initialize hobbies list and Loop to collect hobbies
+hobbies = []
+while True:
+    hobby = input("Enter a favorite hobby or type 'stop' to finish: ")
+    if hobby.lower() == "stop":
+        break
+    hobbies.append(hobby)
