76af4ed69646c0c14c46b82e311dc8e4d80a30e6
Aliaksandr Slizski <aslizski@gmail.com>
2025-11-20 17:41:28 +0100

    нечего добавить. все в коде

--- lecture_2/main.py
+++ lecture_2/main.py
@@ -1,10 +1,1 @@
-def generate_profile(age: int) -> str:
-    # Determine user's life stage based on their age.
-    if 0 <= age <= 12:
-        return "Child"
-    elif 13 <= age <= 19:
-        return "Teenager"
-    elif age >= 20:
-        return "Adult"
-    else:
-        return "Invalid age"
+#хуйня, переделывай
