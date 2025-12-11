6b8a6935f0146cd881ae9b4eb2154a648098d150
Egor <Hardwig65@gmail.com>
2025-12-11 21:31:05 +0100

    feat: add type annotations to all functions

--- lecture_5/book_api/main.py
+++ lecture_5/book_api/main.py
@@ -8,7 +8,7 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-# Создать таблицы, если ещё нет
+# Create table if not exists
 Base.metadata.create_all(bind=engine)
 
 class BookCreate(BaseModel):
